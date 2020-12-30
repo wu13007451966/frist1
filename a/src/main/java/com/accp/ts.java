22a0868b9dbc083bca54162cb64cb393be27642f
wusongbai <1292767075@qq.com>
2020-12-30 09:52:27 +0800

    第一次提交

---
+++ a/src/main/java/com/accp/ts.java
@@ -0,0 +1,8 @@
+package com.accp;
+
+public class ts {
+	public static void show() {
+		System.out.println("Н№гу");
+	}
+
+}
---
+++ b/src/main/java/org/b/test.java
@@ -0,0 +1,11 @@
+package org.b;
+
+import com.accp.ts;
+
+public class test {
+	public static void main(String[] args) {
+		ts.show();
+	}
+	
+
+}
